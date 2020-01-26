6f4ac367ac0fd6d336a65fc107ed29f7c6775cc6
SheepTester <ussa.say@gmail.com>
2020-01-25 19:31:15 -0800

    Screw lodash

--- server/app.js
+++ server/app.js
@@ -1,26 +1,28 @@
-const express = require('express')
-const cors = require('cors')
+(async () => {
+  const express = require('express')
+  const cors = require('cors')
 
-const port = process.env.NODE_ENV === 'production' ? 80 : 3000
+  const port = process.env.NODE_ENV === 'production' ? 80 : 3000
 
-const app = express()
-app.use(express.json())
-app.use(cors())
+  const app = express()
+  app.use(express.json())
+  app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send({ ok: 'probably' })
-})
+  app.get('/', (req, res) => {
+    res.send({ ok: 'probably' })
+  })
 
-app.use('/assassin', require('./assassin.js'))
+  app.use('/assassin', await require('./assassin.js'))
 
-app.use((req, res, next) => {
-  res.status(404).send({ url: req.originalUrl, problem: 'do not know what to do' })
-})
+  app.use((req, res, next) => {
+    res.status(404).send({ url: req.originalUrl, problem: 'do not know what to do' })
+  })
 
-app.use((err, req, res, next) => {
-  res.status(500).send({ url: req.originalUrl, problem: 'brain hurt', history: err.stack })
-})
+  app.use((err, req, res, next) => {
+    res.status(500).send({ url: req.originalUrl, problem: 'brain hurt', history: err.stack })
+  })
 
-app.listen(port, () => {
-  console.log('We are watching.')
-})
+  app.listen(port, () => {
+    console.log('We are watching.')
+  })
+})()
--- server/assassin.js
+++ server/assassin.js
@@ -1,3 +1,5 @@
+module.exports = (async () => {
+
 const express = require('express')
 const router = express.Router()
 
@@ -9,9 +11,12 @@ const path = require('path')
 const low = require('lowdb')
 const FileAsync = require('lowdb/adapters/FileAsync')
 
-const users = low(new FileAsync(path.resolve(__dirname, './db-users.json')))
-const sessions = low(new FileAsync(path.resolve(__dirname, './db-sessions.json')))
-const games = low(new FileAsync(path.resolve(__dirname, './db-games.json')))
+const [usersDB, sessionsDB, gamesDB] = await Promise.all([
+  low(new FileAsync(path.resolve(__dirname, './db-users.json'))),
+  low(new FileAsync(path.resolve(__dirname, './db-sessions.json'))),
+  low(new FileAsync(path.resolve(__dirname, './db-games.json')), { defaultValue: [] })
+])
+const [users, sessions, games] = [usersDB, sessionsDB, gamesDB].map(db => db.value())
 
 function randomID () {
   // I arbitrarily chose 21
@@ -38,37 +43,35 @@ function hashPassword (password, salt) {
 const SESSION_LENGTH = 21 * 86400 * 1000 // 21 days
 function createSession (user) {
   const sessionID = randomID()
-  return sessions
-    .then(db => db
-      .set(sessionID, { user, end: Date.now() + SESSION_LENGTH })
-      .write())
-    .then(() => sessionID)
+  sessions[sessionID] = { user, end: Date.now() + SESSION_LENGTH }
+  return sessionID
 }
 
 function verifySession (sessionID) {
-  return sessions.then(db => {
-    const session = db.get(sessionID).value()
-    if (!session) throw new Error('Fake session!')
-    if (Date.now() > session.end) {
-      return db.unset(sessionID).write()
-        .then(() => Promise.reject(new Error('Session expired!')))
-    }
-    return users.then(db => ({
-      user: db.get(session.user),
-      username: session.user
-    }))
-  })
+  const session = sessions[sessionID]
+  if (!session) throw new Error('Fake session!')
+  if (Date.now() > session.end) {
+    delete sessions[sessionID]
+    // Don't write to database because:
+    // 1. It's not really that important here
+    // 2. This function doesn't return a promise so nothing can then/catch on it.
+    // Something else'll save it later.
+    throw new Error('Session expired!')
+  }
+  return {
+    user: users[session.user],
+    username: session.user
+  }
 }
 
 const usernameRegex = /^[\w-]{3,}$/
 
 // People can spam-create users
 router.post('/join', asyncHandler(async (req, res) => {
-  const usersDB = await users
   const { username, name, password, email } = req.body
   assert(typeof username === 'string')
   assert(usernameRegex.test(username))
-  assert(!usersDB.has(username).value())
+  assert(!users.hasOwnProperty(username))
   assert(typeof name === 'string')
   assert(name.length > 0)
   assert(goodPassword(password))
@@ -76,68 +79,68 @@ router.post('/join', asyncHandler(async (req, res) => {
   assert(email.length > 0)
   const salt = randomID()
   const hashedPassword = await hashPassword(password, salt)
-  const [, sessionID] = await Promise.all([
-    usersDB
-      .set(username, {
-        name,
-        password: hashedPassword,
-        salt,
-        email,
-        bio: '',
-        games: [],
-        myGames: []
-      })
-      .write(),
-    createSession(username)
-  ])
+  users[username] = {
+    name,
+    password: hashedPassword,
+    salt,
+    email,
+    bio: '',
+    games: [],
+    myGames: []
+  }
+  const sessionID = createSession
+  await Promise.all([usersDB.write(), sessionsDB.write()])
   res.send({ session: sessionID })
 }))
 
 router.post('/login', asyncHandler(async (req, res) => {
   const { username, password } = req.body
-  const user = await users.then(db => db.get(username).value())
+  const user = users[username]
   assert(user)
   assert(await hashPassword(password, user.salt) === user.password)
-  res.send({ session: await createSession(username), username })
+  const session = createSession(username)
+  await sessionsDB.write()
+  res.send({ session, username })
 }))
 
 router.post('/logout', asyncHandler(async (req, res) => {
-  await sessions.then(db => db.unset(req.get('X-Session-ID')).write())
+  delete sessions[req.get('X-Session-ID')]
+  await sessionsDB.write()
   res.send({ ok: 'ideally' })
 }))
 
 router.post('/user-settings', asyncHandler(async (req, res) => {
-  const { user } = await verifySession(req.get('X-Session-ID'))
+  const { user } = verifySession(req.get('X-Session-ID'))
   const { name, password, oldPassword, email, bio } = req.body
   if (name) {
     assert(typeof name === 'string')
     assert(name.length > 0)
-    user.set('name', name)
+    user.name = name
   }
   if (password) {
     assert(goodPassword(password))
     // Verify that user knows old password
-    const { salt, password: oldHash } = user.value()
+    const { salt, password: oldHash } = user
     assert(await hashPassword(oldPassword, salt) === oldHash)
-    user.set('password', await hashPassword(password, salt))
+    user.password = await hashPassword(password, salt)
   }
   if (email) {
     assert(typeof email === 'string')
     assert(email.length > 0)
-    user.set('email', email)
+    user.email = email
   }
   if (bio) {
     assert(typeof bio === 'string')
-    user.set('bio', bio)
+    user.bio = bio
   }
-  await user.write()
+  await usersDB.write()
   res.send({ ok: 'if i remember' })
 }))
 
 // Authenticated user data (for user options)
 // TODO: Send games and myGames
 router.get('/user-settings', asyncHandler(async (req, res) => {
-  const { user } = await verifySession(req.get('X-Session-ID'))
+  const { user } = verifySession(req.get('X-Session-ID'))
   const { name, email, bio } = user.value()
   res.send({ name, email, bio })
 }))
@@ -145,60 +148,59 @@ router.get('/user-settings', asyncHandler(async (req, res) => {
 // Public user data (for profiles)
 router.get('/user', asyncHandler(async (req, res) => {
   const { username } = req.query
-  const { name, bio } = await users.then(db => db.get(username).value())
+  const { name, bio } = users[username]
   res.send({ name, bio })
 }))
 
 router.post('/create-game', asyncHandler(async (req, res) => {
-  const { user } = await verifySession(req.get('X-Session-ID'))
+  const { user } = verifySession(req.get('X-Session-ID'))
   const { name, description, password } = req.body
   assert(typeof name === 'string')
   assert(name.length > 0)
   assert(typeof description === 'string')
   assert(typeof password === 'string')
-  const gamesDB = await games
-  const gameID = gamesDB.keys().size().value().toString()
-  assert(!gamesDB.has(gameID).value())
-  await Promise.all([
-    user.get('myGames').push(gameID).write(),
-    gamesDB.set(gameID, {
-      name,
-      description,
-      password,
-      players: {},
-      started: false,
-      ended: false
-    }).write()
-  ])
+  const game = {
+    name,
+    description,
+    password,
+    players: {},
+    started: false,
+    ended: false
+  }
+  games.push(game)
+  const gameID = games.indexOf(game)
+  user.myGames.push(gameID)
+  await Promise.all([usersDB.write(), gamesDB.write()])
   res.send({ game: gameID })
 }))
 
 router.post('/game-settings', asyncHandler(async (req, res) => {
-  const { user } = await verifySession(req.get('X-Session-ID'))
+  const { user } = verifySession(req.get('X-Session-ID'))
   const { game: gameID } = req.query
   const { name, description, password } = req.body
-  const game = await gamesDB.then(db => db.get(gameID))
+  const game = games[gameID]
+  assert(game)
   if (name) {
     assert(typeof name === 'string')
     assert(name.length > 0)
-    game.set('name', name)
+    game.name = name
   }
   if (description) {
     assert(typeof description === 'string')
-    game.set('description', description)
+    game.description = description
   }
   if (password) {
     assert(typeof password === 'string')
-    game.set('password', password)
+    game.password = password
   }
-  await game.write()
+  await gamesDB.write()
   res.send({ ok: 'with luck' })
 }))
 
 router.get('/game-settings', asyncHandler(async (req, res) => {
-  const { user } = await verifySession(req.get('X-Session-ID'))
+  const { user } = verifySession(req.get('X-Session-ID'))
   const { game: gameID } = req.query
-  const game = await gamesDB.then(db => db.get(gameID).value())
+  const game = games[gameID]
   assert(game)
   const { name, description, password, players, started, ended } = game
   res.send({
@@ -213,7 +215,7 @@ router.get('/game-settings', asyncHandler(async (req, res) => {
 
 router.get('/game', asyncHandler(async (req, res) => {
   const { game: gameID } = req.query
-  const game = await gamesDB.then(db => db.get(gameID).value())
+  const game = games[gameID]
   assert(game)
   const { name, description, players, started, ended } = game
   res.send({
@@ -226,40 +228,39 @@ router.get('/game', asyncHandler(async (req, res) => {
 }))
 
 router.post('/join', asyncHandler(async (req, res) => {
-  const { user, username } = await verifySession(req.get('X-Session-ID'))
+  const { user, username } = verifySession(req.get('X-Session-ID'))
   const { game: gameID } = req.query
   const { password } = req.body
-  const game = await gamesDB.then(db => db.get(gameID))
+  const game = games[gameID]
   // Case insensitive
-  assert(game.get('password').toLower().isEqual(password.toLowerCase()).value())
-  await Promise.all([
-    user.get('games').push(game).write(),
-    game.get('players').set(username, {
-      kills: 0,
-      dead: false
-    }).write()
-  ])
+  assert(password.toLowerCase() === game.password.toLowerCase())
+  user.games.push(gameID)
+  game.players[username] = { kills: 0, dead: false }
+  await Promise.all([usersDB.write(), gamesDB.write()])
   res.send({ ok: 'with luck' })
 }))
 
 router.post('/leave', asyncHandler(async (req, res) => {
-  const { user, username } = await verifySession(req.get('X-Session-ID'))
+  const { user, username } = verifySession(req.get('X-Session-ID'))
   const { game: gameID } = req.query
   const { user: target } = req.body
-  const game = await gamesDB.then(db => db.get(gameID))
-  let targetUser = user
+  const game = games[gameID]
+  assert(game)
   let targetUsername = username
+  let targetUser = user
   if (target) {
     // Session user should be owner of the game
-    assert(user.get('myGames').has(gameID))
-    targetUser = usersDB.then(db => db.get(target))
+    assert(user.myGames.includes(gameID))
     targetUsername = target
+    targetUser = users[target]
   }
-  await Promise.all([
-    targetUser.get('games').pull(gameID).write(),
-    game.get('players').unset(targetUsername).write()
-  ])
+  assert(targetUser.games.includes(gameID))
+  targetUser.games.splice(targetUser.games.indexOf(gameID), 1)
+  delete game.players[targetUsername]
+  await Promise.all([usersDB.write(), gamesDB.write()])
   res.send({ ok: 'if i didnt goof' })
 }))
 
-module.exports = router
+return router
+
+})()
