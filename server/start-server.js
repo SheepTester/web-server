90e8b8244e43587527e519741a0728e5ec993038
Sean <ussa.say@gmail.com>
2020-12-09 17:44:54 -0800

    Maybe the child process had an error!

--- server/start-server.js
+++ server/start-server.js
@@ -1,9 +1,13 @@
 const forever = require('forever-monitor')
+const path = require('path')
 
 // https://stackoverflow.com/a/43285131
 const child = new forever.Monitor([/^win/.test(process.platform) ? 'npm.cmd' : 'npm', 'run', 'serve:loop'], {
   silent: false,
-  env: { NODE_ENV: 'production' }
+  env: { NODE_ENV: 'production' },
+  logFile: path.resolve(__dirname, '../public/child-log.txt'),
+  outFile: path.resolve(__dirname, '../public/child-stdout.txt'),
+  errFile: path.resolve(__dirname, '../public/child-stderr.txt')
 })
 
 child.on('restart', () => {
