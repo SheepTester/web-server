b0d459e6937bbe41f8a97d2d21ff66bcd280abcb
Sean <ussa.say@gmail.com>
2022-06-18 16:29:36 -0700

    Use full URL for open graph image

--- server/colour/index.js
+++ server/colour/index.js
@@ -7,13 +7,15 @@ const { asyncHandler } = require('../utils')
 const router = express.Router()
 module.exports = router
 
+const HOSTNAME = 'https://sheep.thingkingland.app'
+
 const hexStrict = /^[0-9a-f]{6}$/
 const hexLoose = /[0-9a-f]{6}|[0-9a-f]{3}/i
 
 router.get('/:colour', (req, res) => {
   const { colour } = req.params
   if (hexStrict.test(colour)) {
-    res.render('colour', { colour })
+    res.render('colour', { colour, host: HOSTNAME })
   } else {
     const match = colour.match(hexLoose)
     if (match) {
