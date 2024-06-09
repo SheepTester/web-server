06e578196ad021251f7c44bc99e4ce3a23803e2b
Sean <ussa.say@gmail.com>
2024-06-09 16:46:13 -0700

    write PID in public/
    there were already some uncommitted changes when i came to edit this repo oop

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const app = require('./app.js')
 
 require('greenlock-express')
@@ -16,5 +17,13 @@ require('greenlock-express')
       httpsServer.close()
     }
     app.onServer(httpsServer)
+    console.log(`HTTPS server is running. [${new Date().toLocaleString()}]`)
   })
   .serve(app)
+
+fs.writeFileSync(
+  path.resolve(__dirname, '../public/last-pid-server.txt'),
+  `[${new Date().toLocaleString('ja-JP', {
+    timeZone: 'America/Los_Angeles'
+  })}] ${process.pid} (server.js)`
+)
--- server/start-server.js
+++ server/start-server.js
@@ -1,5 +1,9 @@
 const forever = require('forever-monitor')
 const path = require('path')
+const fs = require('fs')
+
+// All these Node programs look the same to me
+process.title = 'WEBSERVER'
 
 console.log(`Start [${new Date().toLocaleString()}]`)
 console.error(`Start [${new Date().toLocaleString()}]`)
@@ -8,28 +12,38 @@ console.error(`Start [${new Date().toLocaleString()}]`)
 // But the regex that forever-monitor uses doesn't accept that
 // https://github.com/foreversd/forever-monitor/blob/master/lib/forever-monitor/monitor.js#L517
 // It needs to be quoted. (╯▔皿▔)╯
-const child = new forever.Monitor([/^win/.test(process.platform) ? '"npm.cmd"' : 'npm', 'run', 'serve:loop'], {
-  silent: false,
-  env: { NODE_ENV: 'production' },
-  logFile: path.resolve(__dirname, '../public/child-log.txt'),
-  outFile: path.resolve(__dirname, '../public/child-stdout.txt'),
-  errFile: path.resolve(__dirname, '../public/child-stderr.txt'),
-  // Undocumented option that avoids overwriting the file
-  // https://github.com/foreversd/forever-monitor/blob/master/lib/forever-monitor/plugins/logger.js#L30
-  append: true
-})
+const child = new forever.Monitor(
+  [/^win/.test(process.platform) ? '"npm.cmd"' : 'npm', 'run', 'serve:loop'],
+  {
+    silent: false,
+    env: { NODE_ENV: 'production' },
+    logFile: path.resolve(__dirname, '../public/child-log.txt'),
+    outFile: path.resolve(__dirname, '../public/child-stdout.txt'),
+    errFile: path.resolve(__dirname, '../public/child-stderr.txt'),
+    // Undocumented option that avoids overwriting the file
+    // https://github.com/foreversd/forever-monitor/blob/master/lib/forever-monitor/plugins/logger.js#L30
+    append: true
+  }
+)
 
 child.on('restart', () => {
   console.log(`Restart #${child.times} [${new Date().toLocaleString()}]`)
   console.error(`Restart #${child.times} [${new Date().toLocaleString()}]`)
 })
 
-child.on('exit:code', (code) => {
+child.on('exit:code', code => {
   console.error(`Exit code ${code} [${new Date().toLocaleString()}]`)
 })
 
 child.on('exit', () => {
-  console.log('Okay I\'ll stop now')
+  console.log(`Okay, I'll stop now. [${new Date().toLocaleString()}]`)
 })
+
+fs.writeFileSync(
+  path.resolve(__dirname, '../public/last-pid-start-server.txt'),
+  `[${new Date().toLocaleString('ja-JP', {
+    timeZone: 'America/Los_Angeles'
+  })}] ${process.pid} (start-server.js)`
+)
 
 child.start()
